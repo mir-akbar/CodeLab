9c23272b939bcc686a975adeaf3d55a19aa052c7
Mirakbar <mirakbarkhan29@gmail.com>
2025-01-26 19:26:43 +0530

    uncommented working code

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,12 @@ function App() {
 
   return (
     <Router>
-      {/* <Routes>
+      <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
-      </Routes> */}
-      <Dashboard />
+      </Routes>
+      {/* <Dashboard /> */}
     </Router>
   )
 }
